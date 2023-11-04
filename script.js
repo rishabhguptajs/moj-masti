0bef23881afa7c0d7988a2aae4deda6709545b4b
rishabhguptajs <rishabhgupta4523@gmail.com>
2023-11-05 00:50:22 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var aAudio = new Audio('audio/accha.mp3');
+const img = document.getElementById('img1');
+
+function img1() {
+    aAudio.play();
+}
+alert("Moj masti rukni nahi chahiye, tabhi moj masti me ye kucch bhi bana diya hai, dekh lena, aur accha lage toh tareef kardena, naa lage toh bezzati kardena, that's all, tadaaa😂");
